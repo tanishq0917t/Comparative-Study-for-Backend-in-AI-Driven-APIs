2eb999521c11eada1730811e79a8ca652f60cf26
tanishq0917t <tanishqrawat8@gmail.com>
2025-07-26 13:36:21 +0530

    Changed NodeJS code according to new dataset'

--- express_src/server.js
+++ express_src/server.js
@@ -7,25 +7,11 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 
-// Pre-load ONNX session
 let session;
 ort.InferenceSession.create("../models/iphone_price_predictor.onnx").then((s) => {
   session = s;
   console.log("ONNX model loaded.");
 });
-
-// iPhone version encoding (ordinal)
-const iphoneVersions = [
-  "iPhone 11", "iPhone 11 Pro", "iPhone 12", "iPhone 12 Pro",
-  "iPhone 13", "iPhone 13 Pro", "iPhone 14", "iPhone 14 Pro",
-  "iPhone 15", "iPhone 15 Pro", "iPhone 16", "iPhone 16 Pro"
-];
-
-const encodeVersion = (version) => {
-  const idx = iphoneVersions.indexOf(version);
-  if (idx === -1) throw new Error("Invalid iPhone version");
-  return idx;
-};
 
 app.post("/predict", async (req, res) => {
   if (!session) {
@@ -35,18 +21,23 @@ app.post("/predict", async (req, res) => {
   try {
     const input = req.body;
 
-    // Build individual input tensors matching model expectations
-    const feeds = {
-      battery_health: new ort.Tensor("float32", Float32Array.from([input.battery_health]), [1, 1]),
-      purchase_year: new ort.Tensor("float32", Float32Array.from([input.purchase_year]), [1, 1]),
-      battery_cycles: new ort.Tensor("float32", Float32Array.from([input.battery_cycles]), [1, 1]),
-      damage_percent: new ort.Tensor("float32", Float32Array.from([input.damage_percent]), [1, 1]),
-      iphone_version: new ort.Tensor("string", [iphoneVersions[encodeVersion(input.iphone_version)]], [1, 1])
-    };
+    // Prepare single float32 tensor with shape [1, 5]
+    const inputTensor = new ort.Tensor(
+      "float32",
+      Float32Array.from([
+        input.battery_health,
+        input.purchase_year,
+        input.battery_cycles,
+        input.damage_percent,
+        input.iphone_version_code
+      ]),
+      [1, 5]
+    );
+
+    const feeds = { input: inputTensor };
 
     const results = await session.run(feeds);
 
-    // Get first output (assuming single output)
     const outputKey = Object.keys(results)[0];
     const predictedPrice = results[outputKey].data[0];
 
